93ebbcd8fcf39851b277f4ecf10d934739e4c260
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-04-15 22:25:52 +0500

    Implement PyJWT and PyJWS classes with encoding and decoding functionalities
    - Added PyJWS class for JWS (JSON Web Signature) handling, including methods for encoding and decoding JWTs.
    - Introduced PyJWT class for JWT (JSON Web Token) management, supporting claims validation and token generation.
    - Created exception handling for various JWT-related errors, including signature and audience validation.
    - Implemented utility functions for base64 URL encoding/decoding and PEM format checking.
    - Added JWKSetCache for caching JWK sets with expiration management.
    - Developed PyJWKClient for fetching and managing JWKs from a specified URI.
    - Included a help module for generating bug report information.
    - Established type hints and improved code structure for better maintainability.

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Blogs, User
 from exts import db
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, create_refresh_token
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db.init_app(app)
 
 migrate = Migrate(app, db)
+JWTManager(app)
 
 api = Api(app, doc="/docs")
 
@@ -41,20 +44,47 @@ login_model = api.model(
 
 @api.route("/signup")
 class SignupResource(Resource):
+    @api.marshal_with(signup_model)
     @api.expect(signup_model)
     def post(self):
         data = request.get_json()
+        
+        username=data.get('username')
+
+        db_user = User.query.filter_by(username=username).first()
+        
+        if db_user is not None:
+            return {"message": f"{username} already exists"}, 400
+        
         new_user = User(
             username=data.get("username"),
             email=data.get("email"),
-            password=data.get("password"),
+            password=generate_password_hash(data.get("password")),
         )
         new_user.save()
-        return {"message": "User created successfully"}, 201
+        return new_user, 201
+        # return {"message": "User created successfully"}, 201
     
 @api.route("/login")
 class LoginResource(Resource):
-    pass
+    @api.marshal_with(login_model)
+    @api.expect(login_model)
+    def post(self):
+        data = request.get_json()
+
+        email = data.get("email")
+        password = data.get("password")
+
+        db_user = User.query.filter_by(email=email).first()
+        
+        if db_user and check_password_hash(db_user.password, password):
+            access_token = create_access_token(identity=db_user.email)
+            refresh_token = create_refresh_token(identity=db_user.email)
+
+            return jsonify({
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            })
 
 @api.route("/hello")
 class HelloWorld(Resource):
