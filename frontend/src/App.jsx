79842b0a2b27154c3defe8d3fc977b9d43252ba3
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-04-19 01:15:34 +0500

    refactor: Simplify routing logic in App component by removing authentication checks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,16 @@
-import { Route, Routes, Navigate } from "react-router";
+import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-import { useAuth } from "./auth";
 
 const App = () => {
-  const [logged] = useAuth();
 
   return (
     <Routes>
-      {logged ? (
-        <>
-          {/* Route for the root path */}
-          <Route path="/" element={<Home />} />
-          {/* Redirect any unmatched paths to the home page */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </>
-      ) : (
-        <>
-          {/* Routes for signup and login */}
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          {/* Redirect any unmatched paths to the login page */}
-          <Route path="*" element={<Navigate to="/login" />} />
-        </>
-      )}
+      <Route index element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
-
 export default App;
