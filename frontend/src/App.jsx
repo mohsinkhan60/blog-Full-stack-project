a03ab2132d9f48890a7d06d6bd5448724dfd9147
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-04-19 01:09:41 +0500

    feat: Implement conditional routing based on authentication status in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,32 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
 import Home from "./pages/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import { useAuth } from "./auth";
 
 const App = () => {
+  const [logged] = useAuth();
 
   return (
     <Routes>
-      <Route index element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      {logged ? (
+        <>
+          {/* Route for the root path */}
+          <Route path="/" element={<Home />} />
+          {/* Redirect any unmatched paths to the home page */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </>
+      ) : (
+        <>
+          {/* Routes for signup and login */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          {/* Redirect any unmatched paths to the login page */}
+          <Route path="*" element={<Navigate to="/login" />} />
+        </>
+      )}
     </Routes>
   );
 };
+
 export default App;
