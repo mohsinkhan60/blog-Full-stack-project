dc9dea839dcbb24e0cdf67cdef2974643f2937ae
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-04-19 20:40:27 +0500

    feat: Implement conditional routing based on user authentication state in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
 import Home from "./pages/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const user = useSelector((state) => state.user.user);
 
   return (
     <Routes>
-      <Route index element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      {user ? (
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} /> {/* Redirect unmatched paths to Home */}
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect unmatched paths to Login */}
+        </>
+      )}
     </Routes>
   );
 };
+
 export default App;
